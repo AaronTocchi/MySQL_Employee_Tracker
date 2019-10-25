6947ca907308bb88d9352ad7b771e667d72cc1af
Toki <actocchi@gmail.com>
2019-10-24 18:48:47 -0700

    add role, and add employee working

--- employeeTracker.js
+++ employeeTracker.js
@@ -1,5 +1,7 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+let employees = [];
+let roles = [];
 
 var connection = mysql.createConnection({
     host: "localhost",
@@ -37,12 +39,12 @@ function runInquire() {
                 "Done"
             ]
         })
-        .then(function({ task }) {
+        .then(function ({ task }) {
             switch (task) {
                 case "Add Department":
                     addDept();
                     break;
-                
+
                 case "Add Role":
                     addRole();
                     break;
@@ -72,48 +74,150 @@ function runInquire() {
                     break;
             }
         });
-}
-function addDept(){
+};
+
+function addDept() {
+   
     inquirer
-    .prompt(
-        {
-            name: 'name',
-            message: "What is your department's name?",
-            type: 'input'
+        .prompt(
+            {
+                name: 'name',
+                message: "What is your department's name?",
+                type: 'input'
+            }
+        ).then(function ({ name }) {
+            connection.query(`INSERT INTO department (name) VALUES ('${name}')`, function (err, data) {
+                if (err) throw err;
+                console.log("\x1b[33m", "Department added!")
+                runInquire();
+            })
+        })
+};
+
+function addRole() {
+    let departments = []
+    connection.query(`SELECT * FROM department`, function (err, data) {
+        if (err) throw err;
+        // add dept data to array to pull from for choices
+        for (let i = 0; i < data.length; i++) {
+            departments.push(data[i].name)
         }
-    ).then(function ({ name }) {
-        connection.query(`INSERT INTO department (name) VALUES ('${name}')`, function (err, data) {
+
+
+        inquirer
+            .prompt([
+                {
+                    name: 'title',
+                    message: "What is thier role?",
+                    type: 'input'
+                },
+                {
+                    name: 'salary',
+                    message: 'How much do they make?',
+                    type: 'input'
+                },
+                {
+                    name: 'department_id',
+                    message: 'What department does this role belong to?',
+                    type: 'list',
+                    choices: departments
+                }
+            ]).then(function ({ title, salary, department_id }) {
+                let index = departments.indexOf(department_id)
+
+                connection.query(`INSERT INTO role (title, salary, department_id) VALUES ('${title}', '${salary}', ${index})`, function (err, data) {
+                    if (err) throw err;
+                    console.log("\x1b[33m", "Department added!")
+                    runInquire();
+                })
+            })
+    })
+};
+
+function addEmployee() {
+    connection.query(`SELECT * FROM role`, function (err, data) {
+        if (err) throw err;
+
+
+        for (let i = 0; i < data.length; i++) {
+            roles.push(data[i].title);
+        }
+
+        connection.query(`SELECT * FROM employee`, function (err, data) {
             if (err) throw err;
-            console.log("\x1b[33m","Department added!")
-            runInquire();
+
+            for (let i = 0; i < data.length; i++) {
+                employees.push(data[i].first_name);
+            }
+
+            inquirer
+                .prompt([
+                    {
+                        name: 'first_name',
+                        message: "what's the employees First Name",
+                        type: 'input'
+                    },
+                    {
+                        name: 'last_name',
+                        message: 'What is their last name?',
+                        type: 'input',
+                    },
+                    {
+                        name: 'role_id',
+                        message: 'What is their role?',
+                        type: 'list',
+                        choices: roles,
+                    },
+                    {
+                        name: 'manager_id',
+                        message: "Who is thier manager if they have one?",
+                        type: 'list',
+                        choices: ['none'].concat(employees)
+                    }
+                ]).then(function ({ first_name, last_name, role_id, manager_id }) {
+                    let queryText = `INSERT INTO employee (first_name, last_name, role_id`;
+                    if (manager_id != 'none') {
+                        queryText += `, manager_id) VALUES ('${first_name}', '${last_name}', ${roles.indexOf(role_id)}, ${employees.indexOf(manager_id) + 1})`
+                    } else {
+                        queryText += `) VALUES ('${first_name}', '${last_name}', ${roles.indexOf(role_id) + 1})`
+                    }
+                    console.log(queryText)
+
+                    connection.query(queryText, function (err, data) {
+                        if (err) throw err;
+
+                        runInquire();
+                    })
+                })
+
         })
     })
 };
 
 
 function viewDept() {
-        connection.query(`SELECT * FROM department`, function (err, data) {
-                if (err) throw err;
+    connection.query(`SELECT * FROM department`, function (err, data) {
+        if (err) throw err;
 
-                console.table(data)
-                runInquire();
-            })
+        console.table(data)
+        runInquire();
+    })
 };
 
 function viewRole() {
     connection.query(`SELECT * FROM role`, function (err, data) {
-            if (err) throw err;
+        if (err) throw err;
 
-            console.table(data)
-            runInquire();
-        })
+        console.table(data)
+        runInquire();
+    })
 };
 
 function viewEmployee() {
     connection.query(`SELECT * FROM employee`, function (err, data) {
-            if (err) throw err;
+        if (err) throw err;
 
-            console.table(data)
-            runInquire();
-        })
+        console.table(data)
+        runInquire();
+    })
 };
--- schema.sql
+++ schema.sql
@@ -6,28 +6,24 @@ CREATE DATABASE employee_trackerDB;
 USE employee_trackerDB;
 
 CREATE TABLE department (
-deperatment_id INTEGER(11) AUTO_INCREMENT NOT NULL,
+id INTEGER(11) AUTO_INCREMENT NOT NULL,
 name VARCHAR(10) NOT NULL,
-PRIMARY KEY (deperatment_id)
+PRIMARY KEY (id)
 );
 
 CREATE TABLE role (
-role_id INTEGER(11) AUTO_INCREMENT NOT NULL,
+id INTEGER(11) AUTO_INCREMENT NOT NULL,
 title VARCHAR(30) NOT NULL,
 salary INTEGER(11),
-deperatment_sid INTEGER(11),
-PRIMARY KEY (role_id),
-Foreign Key (deperatment_sid) REFERENCES department(deperatment_id)
-
+department_id INTEGER(11),
+PRIMARY KEY (id)
 );
 
 CREATE TABLE employee (
-employee_id INTEGER(11) AUTO_INCREMENT NOT NULL,
+id INTEGER(11) AUTO_INCREMENT NOT NULL,
 first_name VARCHAR(30) NOT NULL,
 last_name VARCHAR(30),
-role_sid INTEGER(11) NOT NULL,
-manager_sid INTEGER(11),
-PRIMARY KEY (employee_id),
-Foreign Key (role_sid) REFERENCES role(role_id),
-Foreign KEY (manager_sid) REFERENCES employee(employee_id)
+role_id INTEGER(11) NOT NULL,
+manager_id INTEGER(11),
+PRIMARY KEY (id),
 );
